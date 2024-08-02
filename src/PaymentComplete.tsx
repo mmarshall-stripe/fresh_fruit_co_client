98cd7961442ee2a89e752dae884872407e24a2ec
Mark Marshall <mmarshall@stripe.com>
2024-08-02 16:23:28 +0100

    add routes to order complete page

---
+++ src/PaymentComplete.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentComplete = () => {
+  return <h1>Payment Complete!</h1>;
+};
+
+export default PaymentComplete;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import PaymentComplete from "./PaymentComplete";
 import { Theme } from "@twilio-paste/core/theme";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "order-status",
+    element: <PaymentComplete />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Theme.Provider theme="dark">
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </Theme.Provider>
 );
