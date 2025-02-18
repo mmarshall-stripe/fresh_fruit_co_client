979175262d1acd8852e091026b860ca6af0e71b7
Mark Marshall <mmarshall@stripe.com>
2025-02-18 16:46:47 +0000

    point to prod

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -66,6 +66,7 @@ const Checkout = ({
   // Consts
   const stripePromise = loadStripe(
     "pk_live_51PbM05EcWtsOmG7WK9KOQRHGMuK7c7ZST6AgGzyCx7ByX7C5RSARv8CCFyXwHF5OpRgnbC9CkY0FwdzzBaXJ2gYi00KmEFO8Wq"
+    //"pk_test_51PbM05EcWtsOmG7W3xBJtx8L3h4hnmdeB91LISGSB6zaNMbAqfDPFgBBlmnDapRExRRE30lY4Wep9gLlobBhLv4d00GrOpcUN3"
   );
 
   // State
@@ -97,17 +98,21 @@ const Checkout = ({
   // Functions
   const createPaymentIntentGuest = async () => {
     // User is always unknown at the point we create payment intent in this flow
-    // const paymentIntentGuest = await createPaymentIntentRequest(fruitBasket);
-    // if (paymentIntentGuest.error) {
-    // } else {
-    //   const { clientSecret, paymentIntentId } = paymentIntentGuest;
-    //   setClientSecret(clientSecret);
-    //   setPaymentIntentId(paymentIntentId);
-    // }
-    setClientSecret(
-      "pi_3QttZWEcWtsOmG7W1A2YQD6A_secret_tgYTcMH7acYVteMB7rZTIAzyc"
-    );
-    setPaymentIntentId("pi_3QttZWEcWtsOmG7W1A2YQD6A");
+    const paymentIntentGuest = await createPaymentIntentRequest(fruitBasket);
+    if (paymentIntentGuest.error) {
+    } else {
+      const { clientSecret, paymentIntentId } = paymentIntentGuest;
+      setClientSecret(clientSecret);
+      setPaymentIntentId(paymentIntentId);
+    }
+    // setClientSecret(
+    //   // "pi_3QttZWEcWtsOmG7W1A2YQD6A_secret_tgYTcMH7acYVteMB7rZTIAzyc"
+    //   "pi_3QttnlEcWtsOmG7W0FFxiZdz_secret_QeYnUoI3iEBCp34dWMXlxrICw"
+    // );
+    // setPaymentIntentId(
+    //   // "pi_3QttZWEcWtsOmG7W1A2YQD6A"
+    //   "pi_3QttnlEcWtsOmG7W0FFxiZdz"
+    // );
   };
 
   const confirmPaymentIntent = async (
